896ecdec75cde1ae9c959a818b82deda20ecb059
Kaku <ark576@skynet01.skynet.cluster>
2020-05-18 12:30:13 -0400

    Updated utils.py and perform_pred.py to take in images of any size and also to save the predictions in correct orientation

--- perform_pred.py
+++ perform_pred.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import numpy as np
 import os
 import nibabel
+import time
 from models.dense_unet_model import Single_level_densenet, Down_sample, Upsample_n_Concat, Dense_Unet
 # from models.dense_unet_model import *
 from utils import load_data, create_one_hot_seg, back_to_original_4_pred, back_to_original_4_prob, pickling
@@ -52,6 +53,7 @@ args = parser.parse_args()
 
 
 if __name__=='__main__':
+    start_time = time.time()
     if args.use_gpu:
         device = 'cuda'
     else:
@@ -70,9 +72,11 @@ if __name__=='__main__':
     print("Model Loaded, Loading image")
     
     #load image
-    input_image, orient, pad_sh1, sh1 , pad_sh2 , sh2, pad_sh3, sh3 = load_data(args.input_image_path, args.is_mgz)
+    input_image, orient, pad_sh1, sh1 , pad_sh2 , sh2, pad_sh3, sh3, affine_map = load_data(args.input_image_path, args.is_mgz)
     input_image[np.isnan(input_image)] = 0
     input_image = np.clip(input_image, a_min=0.0, a_max = np.max(input_image))
+    
+    print(input_image.shape)
     
     #segments to be removed
     remove_idx_list = [34, 35, 42, 43, 73, 74, 77, 78, 108, 109]
@@ -81,35 +85,35 @@ if __name__=='__main__':
     print("Segmenting the image")
     
     # normalize the image
-    sh2 = input_image.shape[2]
+    sh_2 = input_image.shape[2]
     input_image = torch.from_numpy(input_image).to(device).float()
-    for i in range(sh2):
+    for i in range(sh_2):
         max_value = torch.max(input_image[:,:,i])
         min_value = torch.min(input_image[:,:,i])
         input_image[:,:,i] = (input_image[:,:,i] - min_value)/(max_value - min_value + 1e-4)
         
-    pred_arg_max = [0]*sh2 #stores the prediction
-    pred_prob = [0]*sh2 # stores the softmax prob
-    pred_oh = [0]*sh2
-    input_data = [0]*sh2
-    for i in range(sh2):
+    pred_arg_max = [0]*sh_2 #stores the prediction
+    pred_prob = [0]*sh_2 # stores the softmax prob
+    pred_oh = [0]*sh_2
+    input_data = [0]*sh_2
+    for i in range(sh_2):
         out = model(input_image[:,:,i].unsqueeze(0).unsqueeze(1))
         out = out[:,keep_idx_list,:,:]
-        m = nn.Softmax2d()
-        out = m(out)
-        pred_prob[i] = out.data.cpu().numpy()
+#         m = nn.Softmax2d()
+#         out = m(out)
+#         pred_prob[i] = out.data.cpu().numpy()
         pred_arg_max[i] = torch.argmax(out,dim = 1).data.cpu().numpy()
         segs = out.size()[1]
-        pred_oh[i] = create_one_hot_seg(out,segs)
+#         pred_oh[i] = create_one_hot_seg(out,segs)
         input_data[i] = input_image[:,:,i].unsqueeze(0).data.cpu().numpy()
     
     pred_arg_max = np.moveaxis(np.concatenate(pred_arg_max,axis = 0),0,-1)
-    pred_prob = np.moveaxis(np.concatenate(pred_prob,axis = 0),0,-1)
-    pred_oh = np.moveaxis(np.concatenate(pred_oh, axis = 0),0,-1)
+#     pred_prob = np.moveaxis(np.concatenate(pred_prob,axis = 0),0,-1)
+#     pred_oh = np.moveaxis(np.concatenate(pred_oh, axis = 0),0,-1)
     input_data = np.moveaxis(np.concatenate(input_data, axis = 0),0,-1)
     
     pred_orig = back_to_original_4_pred(pred_arg_max, orient, pad_sh1, sh1 , pad_sh2 , sh2, pad_sh3, sh3)
-    pred_prob_orig = back_to_original_4_prob(pred_prob, orient, pad_sh1, sh1 , pad_sh2 , sh2, pad_sh3, sh3)
+#     pred_prob_orig = back_to_original_4_prob(pred_prob, orient, pad_sh1, sh1 , pad_sh2 , sh2, pad_sh3, sh3)
     
     print("Segmentation Completed, Saving the predictions")
     
@@ -121,11 +125,24 @@ if __name__=='__main__':
         nibabel.save(pred_orig_nib, os.path.join(args.segmentation_dir_path,args.file_name+'_seg.nii.gz'))
         
     print("Predictions saved at ",args.segmentation_dir_path)
+    end_time = time.time()
+    print("Time taken ",(end_time - start_time), " secs")
+    
+    if args.is_mgz:
+        seg = nibabel.freesurfer.MGHImage.from_filename(os.path.join(args.segmentation_dir_path,args.file_name+'_seg.mgz'))
+        if np.sum(seg.affine[:,:3] != affine_map[:,:3]) != 0:
+            pred_orig_nib = nibabel.freesurfer.mghformat.MGHImage(seg.get_data().astype(np.float32), affine_map)
+            nibabel.save(pred_orig_nib, os.path.join(args.segmentation_dir_path,args.file_name+'_seg.mgz'))
+    else:
+        seg = nibabel.load(os.path.join(args.segmentation_dir_path,args.file_name+'_seg.nii.gz'))
+        if np.sum(seg.affine[:,:3] != affine_map[:,:3]) != 0:
+            pred_orig_nib = nibabel.Nifti1Image(seg.get_data(),affine_map)
+            nibabel.save(pred_orig_nib, os.path.join(args.segmentation_dir_path,args.file_name+'_seg.nii.gz'))
         
-    if args.save_prob:
-        print('Saving prob')
-        pickling(pred_prob_orig,os.path.join(args.segmentation_dir_path,args.file_name+'_prob.nii.gz'))
-        print('Prob saved')
+#     if args.save_prob:
+#         print('Saving prob')
+#         pickling(pred_prob_orig,os.path.join(args.segmentation_dir_path,args.file_name+'_prob.nii.gz'))
+#         print('Prob saved')
     
     
     
--- utils.py
+++ utils.py
@@ -31,14 +31,34 @@ def load_data(path, mgz = False):
         t1_img_nii = nibabel.MGHImage.from_filename(path)
     else:
         t1_img_nii = nibabel.load(path)
+    affine_map = t1_img_nii.affine
     t1_img, orientation = orient_correctly(t1_img_nii)
     sh1, sh2, sh3 = t1_img.shape
+    
+    
     dir1_pad = (256 - sh1)//2
+    if dir1_pad < 0:
+        t1_img = t1_img[-dir1_pad:dir1_pad+ sh1%2,:,:]
+    else:
+        t1_img = np.pad(t1_img,((dir1_pad,dir1_pad + sh1%2),(0,0),(0,0)),mode = 'constant',constant_values = 0.0)
+        
+        
     dir2_pad = (256 - sh2)//2
+    if dir2_pad < 0:
+        t1_img = t1_img[:,-dir2_pad:dir2_pad+ sh2%2,:]
+    else:
+        t1_img = np.pad(t1_img,((0,0),(dir2_pad,dir2_pad + sh2%2),(0,0)),mode = 'constant',constant_values = 0.0)
+        
+     
     dir3_pad = (256 - sh3)//2
-    t1_img = np.pad(t1_img,((dir1_pad,dir1_pad + sh1%2),(dir2_pad,dir2_pad + sh2%2),(dir3_pad,dir3_pad + sh3%2)), \
-                    mode = 'constant',constant_values = 0.0)
-    return t1_img,orientation, dir1_pad,sh1,dir2_pad,sh2, dir3_pad, sh3
+    if dir3_pad < 0:
+        t1_img = t1_img[:,:,-dir3_pad:dir3_pad+ sh3%2]
+    else:
+        t1_img = np.pad(t1_img,((0,0),(0,0),(dir3_pad,dir3_pad + sh3%2)),mode = 'constant',constant_values = 0.0)
+        
+#     t1_img = np.pad(t1_img,((dir1_pad,dir1_pad + sh1%2),(dir2_pad,dir2_pad + sh2%2),(dir3_pad,dir3_pad + sh3%2)), \
+#                     mode = 'constant',constant_values = 0.0)
+    return t1_img,orientation, dir1_pad,sh1,dir2_pad,sh2, dir3_pad, sh3, affine_map
 
 def orient_correctly(img_nii):
     orientation = nibabel.io_orientation(img_nii.affine)
@@ -61,8 +81,23 @@ def orient_to_ras(image):
     return img_trans
 
 def back_to_original_4_pred(image,orientation, dir1_pad,sh1, dir2_pad,sh2, dir3_pad,sh3):
-    img_unpadded = image[dir1_pad:dir1_pad+sh1,dir2_pad:dir2_pad+sh2,dir3_pad:dir3_pad+sh3]
-    img_ras = orient_to_ras(img_unpadded)
+    if dir1_pad < 0:
+        image = np.pad(image,((-dir1_pad,-dir1_pad- sh1%2),(0,0),(0,0)),mode = 'constant',constant_values = 0.0)
+    else:
+        image = image[dir1_pad:dir1_pad+sh1,:,:]
+        
+    if dir2_pad < 0:
+        image = np.pad(image,((0,0),(-dir2_pad,-dir2_pad - sh2%2),(0,0)),mode = 'constant',constant_values = 0.0)
+    else:
+        image = image[:,dir2_pad:dir2_pad+sh2,:]
+        
+    if dir3_pad < 0:
+        image = np.pad(image,((0,0),(0,0),(-dir3_pad,-dir3_pad- sh3%2)),mode = 'constant',constant_values = 0.0)
+    else:
+        image = image[:,:,dir3_pad:dir3_pad+sh3]
+    
+#     img_unpadded = image[dir1_pad:dir1_pad+sh1,dir2_pad:dir2_pad+sh2,dir3_pad:dir3_pad+sh3]
+    img_ras = orient_to_ras(image)
     img_orig_orient = np.transpose(img_ras, orientation[:,0].astype(int))
     for k,i in enumerate(orientation[:,1]):
         if i == -1.0:
@@ -71,8 +106,22 @@ def back_to_original_4_pred(image,orientation, dir1_pad,sh1, dir2_pad,sh2, dir3_
     return img_orig_orient
 
 def back_to_original_4_prob(image,orientation, dir1_pad,sh1, dir2_pad,sh2, dir3_pad,sh3):
-    img_unpadded = image[dir1_pad:dir1_pad+sh1,dir2_pad:dir2_pad+sh2,dir3_pad:dir3_pad+sh3,:]
-    img_ras = orient_to_ras(img_unpadded)
+    if dir1_pad < 0:
+        image = np.pad(image,((-dir1_pad,-dir1_pad- sh1%2),(0,0),(0,0),(0,0)),mode = 'constant',constant_values = 0.0)
+    else:
+        image = image[dir1_pad:dir1_pad+sh1,:,:,:]
+        
+    if dir2_pad < 0:
+        image = np.pad(image,((0,0),(-dir2_pad,-dir2_pad - sh2%2),(0,0),(0,0)),mode = 'constant',constant_values = 0.0)
+    else:
+        image = image[:,dir2_pad:dir2_pad+sh2,:,:]
+        
+    if dir3_pad < 0:
+        image = np.pad(image,((0,0),(0,0),(-dir3_pad,-dir3_pad- sh3%2),(0,0)),mode = 'constant',constant_values = 0.0)
+    else:
+        image = image[:,:,dir3_pad:dir3_pad+sh3,:]
+#     img_unpadded = image[dir1_pad:dir1_pad+sh1,dir2_pad:dir2_pad+sh2,dir3_pad:dir3_pad+sh3,:]
+    img_ras = orient_to_ras(image)
     transpose_axis = orientation[:,0].astype(int)
     transpose_axis = np.append(transpose_axis, 3)
     img_orig_orient = np.transpose(img_ras, transpose_axis)
